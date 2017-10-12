396396e9d2dd29e492105754baeb6a07653b0018
Taha Magdy <tahamagdy314@gmail.com>
2017-10-13 00:44:33 +0300

    add getAllPoemsPathsInOnePage

--- scraping the web for poems/scrap.py
+++ scraping the web for poems/scrap.py
@@ -17,3 +17,28 @@ def getBeautifulSoupObjectOfPage(link):
 ###
 # END getBeautifulSoupObjectOfPage
 ###
+
+
+def getAllPoemsPathsInOnePage(baherPageLink):
+    '''
+        * Parameter: The link of a page of a Baher
+        * Returns:   A list of tha peoms links in that page
+    '''
+
+    poemsLinks = []
+
+    beautifulSoupObject = getBeautifulSoupObjectOfPage(baherPageLink)
+    poemsClassName = {"class": "record col-xs-12"}
+    instanceList = beautifulSoupObject.findAll("div", poemsClassName)
+
+    for element in instanceList:
+        anchor = element.find("a")
+        poemsLinks.append(anchor.get("href"))
+
+    print("page > ", len(poemsLinks))
+    return poemsLinks
+###
+# END getAllPeomsLinksOfOneBaher()
+###
+
+
