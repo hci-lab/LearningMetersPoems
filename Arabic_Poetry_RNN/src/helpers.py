5315d499eccf5ed5714146c3cbf225167eda788a
TahaMagdy <tahamagdy314@gmail.com>
2018-01-12 20:04:48 +0200

    add letter_tashkeel separating function [helper]

--- Arabic_Poetry_RNN/src/helpers.py
+++ Arabic_Poetry_RNN/src/helpers.py
@@ -1,5 +1,33 @@
 import arabic
 from itertools import product 
+from pyarabic.araby import strip_tashkeel, strip_tatweel
+
+
+def separate_token_with_dicrites(token):
+    """gets a token with taskeel, and returns a list contains the token characters with their tashkeel.
+
+    Args:
+        param1 (int): list contains the token characters with their tashkeel.
+    Returns:
+         [str]: a list contains the token characters with their tashkeel.
+    """
+
+    token_without_tatweel = strip_tatweel(token)
+    hroof_with_tashkeel = []
+    for index,i in enumerate(token):
+        if token[index] in (arabic.alphabet or arabic.alefat or arabic.hamzat) :
+            k = index
+            harf_with_taskeel =token[index]
+            while((k+1) != len(token) and (token[k + 1] in arabic.tashkeel)):
+                harf_with_taskeel = harf_with_taskeel + "" + token[k + 1]
+                k = k + 1
+            index = k
+            hroof_with_tashkeel.append(harf_with_taskeel)
+    return hroof_with_tashkeel
+
+
+
+
 
 binary = [0,1]
 encoding_combination = [list(i) for i in product([0, 1], repeat=8)]
