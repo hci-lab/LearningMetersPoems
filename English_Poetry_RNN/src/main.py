f04d12bd0f9ce2021ceba1d68f0a41f7512fa16a
moroclash <mmoroclash@gmail.com>
2018-01-16 03:25:36 +0000

    setup Experiment19

--- English_Poetry_RNN/src/main.py
+++ English_Poetry_RNN/src/main.py
@@ -32,20 +32,20 @@ load_weights_flag = 0     #0 or 1
 # 0-> last wait | 1 max val_acc
 last_or_max_val_acc = 0
 
-Experiement_Name = 'Experiment17'
+Experiement_Name = 'Experiment19'
 layer_number = 5
 #if u need one number for all layers add number alone
-n_units = [20]
+n_units = [50]
 # 1->LSTM  , 2->GRU , 3->Bi-LSTM 
 cell_mode = 2
 
 # 0 if you don't need
-drop_out_rate = 0#0.1
+drop_out_rate = 0.1
 test_size_param = 0.1
 validation_split_param = 0.1
 batch_size_param = 64
 
-epochs_param = 100
+epochs_param = 150
 #num of epoch should be wait when monitor don't change
 earlystopping_patience=-1  
 
