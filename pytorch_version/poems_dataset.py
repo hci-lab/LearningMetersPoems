fc186a812e4327c937c6a0cfea72be7a677719c6
TahaMagdy <tahamagdy314@gmail.com>
2018-03-11 20:56:05 +0200

    create the Dataset Hendeler

---
+++ pytorch_version/poems_dataset.py
@@ -0,0 +1,17 @@
+"""Dataset Module for Splitting and Ecnoding"""
+import torch.utils.data.dataset as Dataset
+
+
+class POEMS(Dataset):
+
+    def __init__(self):
+        pass
+    
+
+    def __len__(self):
+        pass
+
+
+
+    def __getitem__(self):
+        pass
