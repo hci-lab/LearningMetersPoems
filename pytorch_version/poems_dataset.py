4f7fb55d585270c18822a77721e77ca8b1c6defc
TahaMagdy <tahamagdy314@gmail.com>
2018-03-12 13:21:52 +0200

    add split_dataDF()

--- pytorch_version/poems_dataset.py
+++ pytorch_version/poems_dataset.py
@@ -17,6 +17,40 @@ class POEMS(Dataset):
         pass
 
 
+    def split_dataDF(self, dataDF, trainFileName, testFileName, test_size=10):
+        '''Splits the hole data to train/test.'''
+    
+        test_DF  = pd.DataFrame(columns=['bayt', 'bahr'])
+        train_DF = pd.DataFrame(columns=['bayt', 'bahr'])
+
+
+        # Raw Labels
+        bohor = dataDF.bahr.unique()
+        
+        for x in bohor:
+
+            bahr_data = dataDF.loc[dataDF['bahr'] == x]
+            bahr_size = len(bahr_data)
+            testing_number = int(bahr_size * (test_size / 100))
+
+            #print(len(bahr_size))
+            #print('{} -> {} '.format(bahr_size, int(testing_number)), bahr_size - int(testing_number))
+
+            # take first N.
+            test_DF   = test_DF.append(bahr_data[:testing_number])
+            bahr_data = bahr_data.drop(bahr_data.index[:testing_number])
+            train_DF  = train_DF.append(bahr_data)
+
+        try:
+            if self.check_existance():
+                print('train/test already exist')
+            else:
+                train_DF.to_csv(trainFileName)
+                test_DF.to_csv(testFileName)
+        except RuntimeError:
+            raise RuntimeError('train/test csv are not created!')
+
+
     def check_existance(self):
         return os.path.exists(self.train_csv_file) and\
                os.path.exists(self.test_csv_file)
